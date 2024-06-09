4925a2896a9bd66db4903f0fc8512998311fcd9c
Bryan <31880091+raizdev@users.noreply.github.com>
2024-06-09 02:10:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -88,9 +88,8 @@ then
   read -p "Mikrotik API password: " mikrotik_password
   sed -i "s/mktxp_user_password/$mikrotik_password/g" ./mktxp/mktxp.conf
 else
-  sed -i.bak -e '95,103d' ./docker-compose.yml
+  sed -i.bak -e '86,93d' ./docker-compose.yml
   rm -rf ./grafana/provisioning/dashboards/isp_monitoring/mikrotik.json
-  sed -i.bak -e '22,33d' ./prometheus/prometheus.yml
   rm -rf ./mktxp
 fi
 
